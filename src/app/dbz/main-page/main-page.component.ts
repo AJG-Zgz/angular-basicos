e9608b0ca9a957667885ec5d4fcf9b80a73812f0
Angel J. Grauliana <ajgrauliana@gmail.com>
2022-05-19 20:54:04 +0200

    Modulo DbzModule

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interfaces';
+import { DbzService } from '../services/dbz.services';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent {
 
-  constructor() { }
+  nuevoPersonaje: Personaje = {
+    nombre: 'Maestro Roshi',
+    poder: 1000
+  }
 
-  ngOnInit(): void {
+  constructor() {
+
   }
 
 }
